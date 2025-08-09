a7111012b3625f0b02003f82be470c7a9dd0a1a4
Manuel Couto <mcouto1@mail.austral.edu.ar>
2025-08-09 19:40:58 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,18 +1,19 @@
-a=int(input("Ingrese el valor de a: "))
-b=int(input("Ingrese el valor de b: "))
+def math():
+    a=57
+    b=7
 
-suma=a+b
-print(suma)
-dif=a-b
-print(dif)
-prod=a*b
-print(prod)
-prom=(a+b)/2
-print(prom)
-coc=int(a/b)
-print(coc)
-res=a%b
-print(res)
-div=a/b
-print(div)
+    suma=a+b
+    print(suma)
+    dif=a-b
+    print(dif)
+    prod=a*b
+    print(prod)
+    prom=(a+b)/2
+    print(prom)
+    coc=int(a/b)
+    print(coc)
+    res=a%b
+    print(res)
+    div=a/b
+    print(div)
 
